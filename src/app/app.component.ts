580b8845f59c9105029375f94423a39bbeb24b44
Ahmed Atef <Ahmedatef32003@gmail.com>
2026-01-04 12:46:58 +0200

    upload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Editor } from 'primeng/editor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, Editor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'editor';
+  text: string | undefined;
+
+  @ViewChild('el') editor!: ElementRef;
+
+  click() {
+    this.editor.nativeElement.innerHTML = this.text;
+  }
 }
+
